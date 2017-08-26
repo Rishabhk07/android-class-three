c07ec63f7c41f7f83ad186c87f96fea98813191f
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-08-26 17:18:59 +0530

    result code
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/com/codingblocks/thidandroidclass/MainActivity.java
+++ app/src/main/java/com/codingblocks/thidandroidclass/MainActivity.java
@@ -3,6 +3,7 @@ package com.codingblocks.thidandroidclass;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
     EditText etValue;
+    public static final String TAG = "Main";
+    public static final int REQ_CODE  =  123;
+    public static final String KEY = "key";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +27,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,SecondActivity.class);
                 String val = etValue.getText().toString();
-                i.putExtra("key",val);
-                startActivity(i);
-
+                i.putExtra(KEY,val);
+                startActivityForResult(i,REQ_CODE);
             }
         });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MainActivity.REQ_CODE&& resultCode == 789){
+            Log.d(TAG, "onActivityResult: " + data.getStringExtra("second"));
+        }
     }
 }
--- app/src/main/java/com/codingblocks/thidandroidclass/SecondActivity.java
+++ app/src/main/java/com/codingblocks/thidandroidclass/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.codingblocks.thidandroidclass;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,8 +12,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        String data = getIntent().getStringExtra("key");
+        String data = getIntent().getStringExtra(MainActivity.KEY);
         textView = (TextView) findViewById(R.id.tvParamter);
         textView.setText(data);
+        data = data + " Rishabh Khanna";
+        Intent i = new Intent();
+        i.putExtra("second",data);
+
+        Intent j = new Intent();
+        setResult(789,i);
+        setResult(567,j);
+
     }
 }
